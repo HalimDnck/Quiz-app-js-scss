4933267709477fe130f0cbfa1ed748d158bc3ae9
Halim Dinckalmis <halimdinc0@gmail.com>
2023-03-14 05:31:12 +0300

    js update2

--- js/game.js
+++ js/game.js
@@ -1,11 +1,58 @@
+//VARS
+const questionText = document.querySelector(".question");
+let answerButton = [];
+answerButton = document.querySelectorAll('.answer-container > button');
+const container = document.querySelector(".answer-container");
+
+let startIndex;
+let questionList = [];
+let firstQuestion;
+let optionRandom;
 
 
-//Fonksiyonlar
-function nextQuestion(){
 
-}
 
-function checkAnswer(){
+//Fetch Questions
+fetch("questions/questions.json").then(
+    response => response.json()
+).then(responseJson => {
 
-}
+
+
+    nextQuestion();
+    function nextQuestion(){
+        container.style.display = "flex";
+        startIndex = Math.floor(Math.random() * 20);
+    
+        firstQuestion = responseJson.questions[startIndex];
+
+        questionText.innerHTML = firstQuestion.question;
+        
+        optionRandom = Math.floor(Math.random() * 4);
+
+        for(let i=0; i < 3; i++){
+            let listofElements = [0,1,2,3,0,1,2,3,0,1,2,3];
+            answerButton[listofElements[optionRandom+i]].innerText = firstQuestion.incorrect_answers[i];
+        }
+
+        if(optionRandom==0){
+            answerButton[3].innerText = firstQuestion.correct_answer;
+            answerButton[3].classList.add("true");
+        }
+        else{
+            answerButton[optionRandom-1].innerText = firstQuestion.correct_answer;
+            answerButton[optionRandom-1].classList.add("true");
+        }
+    }
+
+    
+
+    
+    
+   
+
+
+})
+
+
 
